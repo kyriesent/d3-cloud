7a9dc398991183736301fc70759bcbd0bbbd9c52
David Jsa <ddjsa@umich.edu>
2014-10-12 22:30:38 -0400

    Use global canvas

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var Canvas = require("canvas"),
-    globals = {};
+// var Canvas = require("canvas"),
+//     globals = {};
 
-// stash globals
-if ("Canvas" in global) globals.Canvas = global.Canvas;
-global.Canvas = Canvas;
+// // stash globals
+// if ("Canvas" in global) globals.Canvas = global.Canvas;
+// global.Canvas = Canvas;
 
 module.exports = require("./d3.layout.cloud");
 
-// restore globals
-if ("Canvas" in globals) global.Canvas = globals.Canvas;
-else delete global.Canvas;
+// // restore globals
+// if ("Canvas" in globals) global.Canvas = globals.Canvas;
+// else delete global.Canvas;
